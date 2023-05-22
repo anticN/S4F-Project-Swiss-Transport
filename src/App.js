64d1846e6e1d3d6d8b991e0f4926bd4965dc8cf6
anticN <anticn@bzz.ch>
2023-05-22 23:11:12 +0200

    add: active buttons but not finished

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = event => {
+    setIsActive(current => !current);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Schweizer öffentlicher Verkehr</h1>
       </header>
-      <div className='Choice'>
-        <button className='choiceButton'>Ort</button>
-        <button className='choiceButton'>Abfahrten</button>
-        <button className='choiceButton'>Verbindungen</button>
-      </div>
-      <div className='inputChoice'>
-        <form>
-          <label for="location">Ort</label>
-          <input type='text' name='location' className='inputfield' id='location' placeholder='Name des Ortes um die Stationen anzuzeigen'></input>
-          <label for="date">Datum</label>
-          <input type='text' name='date' className="datetimefield" id='date' placeholder='yyyy-mm-dd' defaultValue={new Date().toISOString().slice(0,10)}></input>
-          <label for="time">Zeit</label>
-          <input type='text' name='time' className="datetimefield" id='time' placeholder='hh:mm' defaultValue={new Date().toISOString().slice(11,16)}></input>
-          <button className='submitButton' type='submit'>Suchen</button>
-        </form>
-      </div>
+      <div className='main'>
+        <div className='Choice'>
+          <button className={isActive ? "activeChoiceButton" : "choiceButton"} onClick={handleClick}>Abfahrten</button>
+          <button className={isActive ? "choiceButton" : "activeChoiceButton"} onclick={handleClick}>Verbindungen</button>
+        </div>
+        <div className='inputChoice'>
+          <form>
+            <label for="station">Station</label>
+            <input type='text' name='station' className='inputfield' id='station' placeholder='Name der Station um die Abfahrten anzuzeigen'></input>
+            <label for="date">Datum</label>
+            <input type='text' name='date' className="datetimefield" id='date' placeholder='yyyy-mm-dd' defaultValue={new Date().toISOString().slice(0,10)}></input>
+            <label for="time">Zeit</label>
+            <input type='text' name='time' className="datetimefield" id='time' placeholder='hh:mm' defaultValue={new Date().toISOString().slice(11,16)}></input>
+            <button className='submitButton' type='submit'>Suchen</button>
+          </form>
+        </div>
+        </div>
     </div>
   );
 }
