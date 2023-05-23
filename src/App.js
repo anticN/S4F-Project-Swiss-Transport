52ea4823efcdf526b99cd3fe03ced8966c921493
anticN <anticn@bzz.ch>
2023-05-23 21:47:16 +0200

    fix: change form onClick with styling

--- src/App.js
+++ src/App.js
@@ -5,42 +5,25 @@ import './App.css';
 function App() {
   const [activeButton, setActiveButton] = useState("button1");
   const [content, setContent] = useState(`
-    <form className="forms">
         <label for="station">Station</label>
-        <input type='text' name='station' className='inputfield' id='station' placeholder='Name der Station um die Abfahrten anzuzeigen'></input>
-        <label for="date">Datum</label>
-        <input type='text' name='date' className="datetimefield" id='date' placeholder='yyyy-mm-dd' defaultValue={new Date().toISOString().slice(0,10)}></input>
-        <label for="time">Zeit</label>
-        <input type='text' name='time' className="datetimefield" id='time' placeholder='hh:mm' defaultValue={new Date().toISOString().slice(11,16)}></input>
-      <button className='submitButton' type='submit'>Suchen</button>
-  </form>`);
+        <input type='text' name='station' class='inputfield' id='station' placeholder='Name der Station um die Abfahrten anzuzeigen'></input>`
+        );
 
   const handleClick = (button) => {
     setActiveButton(button);
 
     if (button === 'button1') {
       setContent(`
-      <form>
         <label for="station">Station</label>
-        <input type='text' name='station' className='inputfield' id='station' placeholder='Name der Station um die Abfahrten anzuzeigen'></input>
-        <label for="date">Datum</label>
-        <input type='text' name='date' className="datetimefield" id='date' placeholder='yyyy-mm-dd' defaultValue={new Date().toISOString().slice(0,10)}></input>
-        <label for="time">Zeit</label>
-        <input type='text' name='time' className="datetimefield" id='time' placeholder='hh:mm' defaultValue={new Date().toISOString().slice(11,16)}></input>
-    <button className={'submitButton'} type='submit'>Suchen</button>
-  </form>`);
+        <input type='text' name='station' class='inputfield' id='station' placeholder='Name der Station um die Abfahrten anzuzeigen'></input>
+        `);
     }else if (button === 'button2') {
-      setContent(`<form>
+      setContent(`
       <label for="depart">Abfahrt</label>
-        <input type='text' name='depart' className='inputfield' id='depart' placeholder='Name des Abfahrtorts'></input>
+        <input type='text' name='depart' class='inputfield2' id='depart' placeholder='Name des Abfahrtorts'></input>
       <label for="arrive">Ankunft</label>
-        <input type='text' name='arrive' className='inputfield' id='arrive' placeholder='Name des Ankunftsorts'></input>
-      <label for="date">Datum</label>
-        <input type='text' name='date' className="datetimefield" id='date' placeholder='yyyy-mm-dd' defaultValue={new Date().toISOString().slice(0,10)}></input>
-      <label for="time">Zeit</label>
-        <input type='text' name='time' className="datetimefield" id='time' placeholder='hh:mm' defaultValue={new Date().toISOString().slice(11,16)}></input>
-    <button className='submitButton' type='submit'>Suchen</button>
-  </form>`)
+        <input type='text' name='arrive' class='inputfield2' id='arrive' placeholder='Name des Ankunftsorts'></input>
+      `)
     }
   };
 
@@ -54,9 +37,18 @@ function App() {
           <button onClick={() => handleClick("button1")} className={activeButton === "button1" ? "active" : "choiceButton"} >Abfahrten</button>
           <button onClick={() => handleClick("button2")} className={activeButton === "button2" ? "active" : "choiceButton"}>Verbindungen</button>
         </div>
-        <div className='inputChoice content' dangerouslySetInnerHTML={{ __html: content}} />
+        <div className='inputChoice'>
+          <form>
+            <div className="content" dangerouslySetInnerHTML={{ __html: content}} />
+            <label for="date">Datum</label>
+            <input type='text' name='date' className="datetimefield" id='date' placeholder='yyyy-mm-dd' defaultValue={new Date().toISOString().slice(0,10)}></input>
+            <label for="time">Zeit</label>
+            <input type='text' name='time' className="datetimefield" id='time' placeholder='hh:mm' defaultValue={new Date().toISOString().slice(11,16)}></input>
+            <button className='submitButton' type='submit'>Suchen</button>
+          </form>
+          </div>
         </div>
-    </div>
+      </div>
   );
 }
 
